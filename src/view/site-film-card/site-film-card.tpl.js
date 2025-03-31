2fb01db19000d039d8590c9ed82697b3d3334d30
jar <dev.bazylev@gmail.com>
2025-03-31 15:06:06 +0300

    refactor: split components

--- src/view/site-film-card/site-film-card.tpl.js
+++ src/view/site-film-card/site-film-card.tpl.js
@@ -22,17 +22,3 @@ export const createFilmCard = (film) => {
             </div>
           </article>`;
 };
-
-export const createFilmListTemplate = (filmList, isExtra) => {
-  const filmsListComponent = filmList.map((film) => createFilmCard(film)).join('');
-
-  return `<section class="films-list ${ isExtra ? 'films-list--extra' : ''}">
-            <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-
-            <div class="films-list__container">
-              ${ filmsListComponent }
-            </div>
-
-            ${ isExtra ? '<button class="films-list__show-more">Show more</button>' : ''}
-          </section>`;
-};
---
+++ src/view/site-film-list/site-films-list.tpl.js
@@ -0,0 +1,3 @@
+export const createFilmsListTemplate = (title, isExtra) => `<section class="films-list ${ isExtra ? 'films-list--extra' : ''}">
+    <h2 class="films-list__title ${ !isExtra ? 'visually-hidden' : ''}">${ title }</h2>
+  </section>`;
