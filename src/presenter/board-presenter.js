889f9e4c3459d57d5cdc5256805b3baf3b04e2a6
jar <dev.bazylev@gmail.com>
2025-03-31 15:43:45 +0300

    feat: add data to popup

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,13 +1,14 @@
 import { FilmCardsOnPage, FilterType, SortType } from '../consts';
-import { render } from '../framework/render';
+import { remove, render } from '../framework/render';
 import SiteFilmCardView from '../view/site-film-card/site-film-card-view';
 import SiteFilmListContainerView from '../view/site-film-list-container/site-film-list-container-view';
 import SiteFilmsListView from '../view/site-film-list/site-films-list-view';
+import SiteFilmPopupView from '../view/site-film-popup/site-film-popup-view';
 import SiteFilmsContainerView from '../view/site-films-container/site-films-container-view';
 import SiteFiltersView from '../view/site-filters/site-filters-view';
 import SiteSortView from '../view/site-sort/site-sort-view';
-
 export default class BoardPresenter {
+
   #filmsContainerComponent = new SiteFilmsContainerView();
   #allFilmsContainer = new SiteFilmsListView();
   #filmListContainerComponent = new SiteFilmListContainerView();
@@ -23,6 +24,7 @@ export default class BoardPresenter {
     render(new SiteFiltersView(FilterType.ALL), this.boardContainer);
     render(new SiteSortView(SortType.DEFAULT), this.boardContainer);
     render(this.#filmsContainerComponent, this.boardContainer);
+
     this.#renderAllFilms();
   }
 
@@ -35,7 +37,34 @@ export default class BoardPresenter {
   }
 
   #renderFilm (film) {
-    const filmComponent = new SiteFilmCardView(film);
+    let filmPopup = null;
+
+    const escKeyDownHandler = (evt) => {
+      if (evt.key === 'Escape') {
+        evt.preventDefault();
+        removePopup();
+        document.removeEventListener('keydown', escKeyDownHandler);
+      }
+    };
+
+    function removePopup() {
+      remove(filmPopup);
+    }
+
+    function renderPopup() {
+      filmPopup = new SiteFilmPopupView(film);
+
+      render(filmPopup, document.body);
+    }
+
+    const filmComponent = new SiteFilmCardView({
+      film,
+      onFilmCardClick: () => {
+        renderPopup();
+        document.addEventListener('keydown', escKeyDownHandler);
+      }
+    });
+
     render(filmComponent, this.#filmListContainerComponent.element);
   }
 }
