0710a39e4f5c4055d4149b7d020afb9f823be789
jar <dev.bazylev@gmail.com>
2025-04-03 17:58:09 +0300

    feat: add listeners && add sorting && refactor

--- src/view/site-film-list/site-films-list-view.js
+++ src/view/site-film-list/site-films-list-view.js
@@ -2,12 +2,16 @@ import AbstractView from '../../framework/view/abstract-view';
 import { createFilmsListTemplate } from './site-films-list.tpl';
 
 export default class SiteFilmsListView extends AbstractView {
-  constructor ({
+
+  constructor () {
+    super();
+  }
+
+  init ({
     title = 'All movies. Upcoming',
     isEmptyList = false,
     isExtra = false,
   } = {}) {
-    super();
     this.title = title;
     this.isEmptyList = isEmptyList;
     this.isExtra = isExtra;
